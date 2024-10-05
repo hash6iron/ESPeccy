7d06744f553837e2d905f740948d8db4b15b8235
splintergu <splintergu@gmail.com>
2024-10-05 19:22:49 -0300

    Fix .SP files swapped AF and AF'

--- include/commit_info.h
+++ include/commit_info.h
@@ -1,6 +1,6 @@
 #ifndef COMMIT_INFO_H
 #define COMMIT_INFO_H
 
-const char* COMMIT_DATE = "2410051522";  // Formato YYmmddHHMM
+const char* COMMIT_DATE = "2410051923";  // Formato YYmmddHHMM
 
 #endif // COMMIT_INFO_H
