da8e4a523df187cec944af3b7739fd47df80eb83
splintergu <splintergu@gmail.com>
2024-10-08 18:45:09 -0300

    Cursor menu navigation (default mode)

--- include/commit_info.h
+++ include/commit_info.h
@@ -1,6 +1,6 @@
 #ifndef COMMIT_INFO_H
 #define COMMIT_INFO_H
 
-const char* COMMIT_DATE = "2410081536";  // Formato YYmmddHHMM
+const char* COMMIT_DATE = "2410081845";  // Formato YYmmddHHMM
 
 #endif // COMMIT_INFO_H
